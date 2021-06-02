71bf7a5e5710fcfdb972d29d704c416b3bf4c842
Kael <kaelwd@gmail.com>
2021-06-03 01:55:37 +1000

    feat: add router integration

--- packages/vuetify/build/webpack.base.config.js
+++ packages/vuetify/build/webpack.base.config.js
@@ -75,7 +75,10 @@ module.exports = {
       typescript: {
         configFile: resolve('../tsconfig.checks.json'),
         mode: 'write-references'
-      }
+      },
+      logger: {
+        devServer: false,
+      },
     }),
   ],
   performance: {
